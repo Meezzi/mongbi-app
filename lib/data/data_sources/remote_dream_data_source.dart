0dbad03b0c8288289ca0e30f0c2b81ed2ff4ddf4
Meezzi <alswl-3016@naver.com>
2025-06-05 09:25:30 +0900

    feat: 꿈을 서버에 저장하고, 성공/ 실패 분기를 처리하는 saveDream 메서드 추가

--- lib/data/data_sources/remote_dream_data_source.dart
+++ lib/data/data_sources/remote_dream_data_source.dart
@@ -1,7 +1,17 @@
 import 'package:dio/dio.dart';
+import 'package:mongbi_app/data/dtos/dream_dto.dart';
 
 class RemoteDreamDataSource {
   RemoteDreamDataSource(this.dio);
 
   final Dio dio;
+
+  Future<bool> saveDream(DreamDto dream) async {
+    final response = await dio.post('/dreams', data: dream.toJson());
+    if (response.statusCode == 201 && response.data['success'] == true) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
