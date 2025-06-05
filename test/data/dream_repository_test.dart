9c89cb720cc0f31f0839951a78fc67b9f1784017
Meezzi <alswl-3016@naver.com>
2025-06-05 12:02:40 +0900

    test: Dream Repository saveDream을 테스트하기 위한 setUp 초기화

--- test/data/dream_repository_test.dart
+++ test/data/dream_repository_test.dart
@@ -1,6 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:mongbi_app/data/data_sources/dream_data_source.dart';
+import 'package:mongbi_app/data/dtos/dream_dto.dart';
 
-void main() {}
+void main() {
+  DreamDataSource? dreamDataSource;
+  RemoteDreamRepository? remoteDreamRepository;
+
+  group('Dream Repository SaveDream Test', () {
+    setUp(() {
+      dreamDataSource = MockDreamDataSource();
+      remoteDreamRepository = RemoteDreamRepository(dreamDataSource);
+    });
+  });
+}
 
 class MockDreamDataSource extends Mock implements DreamDataSource {}
