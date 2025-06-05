ebc688e6d712ee6153c93cc52da49e8b35ffd858
Meezzi <alswl-3016@naver.com>
2025-06-05 19:17:28 +0900

    feat: DreamRepository interface 생성 후, Repository에서 implements

--- lib/data/repositories/remote_dream_repository.dart
+++ lib/data/repositories/remote_dream_repository.dart
@@ -1,12 +1,14 @@
 import 'package:mongbi_app/data/data_sources/dream_data_source.dart';
 import 'package:mongbi_app/data/dtos/dream_dto.dart';
 import 'package:mongbi_app/domain/entities/dream.dart';
+import 'package:mongbi_app/domain/repositories/dream_repository.dart';
 
-class RemoteDreamRepository {
+class RemoteDreamRepository implements DreamRepository {
   RemoteDreamRepository(this.dreamDataSource);
 
   final DreamDataSource dreamDataSource;
 
+  @override
   Future<bool> saveDream(Dream dream) async {
     final dreamDto = DreamDto.fromEntity(dream);
     return await dreamDataSource.saveDream(dreamDto);
