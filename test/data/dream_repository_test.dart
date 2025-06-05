c8497626acec526264f4c1f98066b59e6ee77080
Meezzi <alswl-3016@naver.com>
2025-06-05 12:00:45 +0900

    test: DreamRepository를 테스트하기 위한 DreamDataSource를 Mock 객체로 생성

---
+++ test/data/dream_repository_test.dart
@@ -0,0 +1,6 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:mongbi_app/data/data_sources/dream_data_source.dart';
+
+void main() {}
+
+class MockDreamDataSource extends Mock implements DreamDataSource {}
