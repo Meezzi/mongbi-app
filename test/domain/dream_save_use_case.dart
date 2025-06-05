05644c3e3a3499bc05f525b5ab7f33acb11d3050
Meezzi <alswl-3016@naver.com>
2025-06-05 20:35:19 +0900

    test: DreamSaveUseCase를 테스트하기 위한 MockDreamRepository 생성

---
+++ test/domain/dream_save_use_case.dart
@@ -0,0 +1,7 @@
+
+import 'package:mocktail/mocktail.dart';
+import 'package:mongbi_app/domain/repositories/dream_repository.dart';
+
+void main() {}
+
+class MockDreamRepository extends Mock implements DreamRepository {}
