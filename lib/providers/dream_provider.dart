ace7da3324a8ab2a5ae4f7489fea0508c6886d44
Meezzi <alswl-3016@naver.com>
2025-06-05 10:18:12 +0900

    feat: dreamDataSourceProvider 생성

--- lib/providers/dream_provider.dart
+++ lib/providers/dream_provider.dart
@@ -1,7 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mongbi_app/data/data_sources/dream_data_source.dart';
+import 'package:mongbi_app/data/data_sources/remote_dream_data_source.dart';
 
 final dioProvider = Provider<Dio>(
   (ref) => Dio(BaseOptions(baseUrl: dotenv.env['BASE_URL']!)),
+);
+
+final _dreamDataSourceProvider = Provider<DreamDataSource>(
+  (ref) => RemoteDreamDataSource(ref.read(dioProvider)),
 );
