5754c48f8b37f23599767aa7a2aa4d84d77f758f
Meezzi <alswl-3016@naver.com>
2025-06-09 17:43:08 +0900

    feat: analyzeDream 메서드 구현

--- lib/data/repositories/remote_dream_repository.dart
+++ lib/data/repositories/remote_dream_repository.dart
@@ -1,16 +1,23 @@
+import 'package:mongbi_app/data/data_sources/dream_analysis_data_source.dart';
 import 'package:mongbi_app/data/data_sources/dream_data_source.dart';
 import 'package:mongbi_app/data/dtos/dream_dto.dart';
 import 'package:mongbi_app/domain/entities/dream.dart';
 import 'package:mongbi_app/domain/repositories/dream_repository.dart';
 
 class RemoteDreamRepository implements DreamRepository {
-  RemoteDreamRepository(this.dreamDataSource);
+  RemoteDreamRepository(this.dreamDataSource, this.dreamAnalysisDataSource);
 
   final DreamDataSource dreamDataSource;
+  final DreamAnalysisDataSource dreamAnalysisDataSource;
 
   @override
   Future<bool> saveDream(Dream dream) async {
     final dreamDto = DreamDto.fromEntity(dream);
     return await dreamDataSource.saveDream(dreamDto);
+  }
+
+  @override
+  Future<String> analyzeDream(String dreamContent, int dreamScore) async {
+    return await dreamAnalysisDataSource.analyzeDream(dreamContent, dreamScore);
   }
 }
