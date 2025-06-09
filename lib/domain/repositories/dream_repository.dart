0b7c59075b71b25231f313e7e08b8c9a97c97202
Meezzi <alswl-3016@naver.com>
2025-06-09 17:42:44 +0900

    feat: 꿈 해석 결과를 반환하는 analyzeDream 인터페이스 추가

--- lib/domain/repositories/dream_repository.dart
+++ lib/domain/repositories/dream_repository.dart
@@ -2,4 +2,6 @@ import 'package:mongbi_app/domain/entities/dream.dart';
 
 abstract interface class DreamRepository {
   Future<bool> saveDream(Dream dream);
+
+  Future<String> analyzeDream(String dreamContent, int dreamScore);
 }
