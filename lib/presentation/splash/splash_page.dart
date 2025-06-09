a2c87a1b730ddfe74eeba387e9d7a97ce89a7ba8
MoonHo <75512752+monho@users.noreply.github.com>
2025-06-09 12:55:18 +0900

    fix: star.svg -> star.png로 변경

--- lib/presentation/splash/splash_page.dart
+++ lib/presentation/splash/splash_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
@@ -7,20 +6,30 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color(0xFF08063A),
-              Color(0xFF3B7EBA),
-              Color(0xFF3FAEF4), 
-              Color(0xFF9AE4D6),
-            ],
-            stops: [0.2, 0.42, 0.72, 0.93],
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFF08063A),
+                  Color(0xFF3B7EBA),
+                  Color(0xFF3FAEF4),
+                  Color(0xFF9AE4D6),
+                ],
+                stops: [0.2, 0.42, 0.72, 0.93],
+              ),
+            ),
           ),
-        ),
+          Image.asset(
+            'assets/images/star.png',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+        ],
       ),
     );
   }
