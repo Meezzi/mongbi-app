9000161694db3c21eadf7bdf71aba7a0f5f4e5ad
Meezzi <alswl-3016@naver.com>
2025-06-09 16:39:52 +0900

    feat: 꿈 해석 Provider 생성

--- lib/providers/dream_provider.dart
+++ lib/providers/dream_provider.dart
@@ -1,5 +1,8 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mongbi_app/data/data_sources/dream_analysis_data_source.dart';
 import 'package:mongbi_app/data/data_sources/dream_data_source.dart';
+import 'package:mongbi_app/data/data_sources/remote_dream_analysis_data_source.dart';
 import 'package:mongbi_app/data/data_sources/remote_dream_data_source.dart';
 import 'package:mongbi_app/data/repositories/remote_dream_repository.dart';
 import 'package:mongbi_app/domain/repositories/dream_repository.dart';
@@ -13,6 +16,15 @@ final _dreamDataSourceProvider = Provider<DreamDataSource>(
 
 final _dreamRepositoryProvider = Provider<DreamRepository>(
   (ref) => RemoteDreamRepository(ref.read(_dreamDataSourceProvider)),
+);
+
+// 꿈 해석 Provider
+final _dreamAnalysisDataSource = Provider<DreamAnalysisDataSource>(
+  (ref) => RemoteDreamAnalysisDataSourceImpl(
+    dio: ref.read(dioProvider),
+    apiKey: dotenv.env['CLAUDE_API_KEY']!,
+    baseUrl: dotenv.env['CLAUDE_URL']!,
+  ),
 );
 
 final dreamWriteViewModelProvider =
