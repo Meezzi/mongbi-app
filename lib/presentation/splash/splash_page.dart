7c829f31bd1e49866e245af6edb6245381b52dc6
MoonHo <75512752+monho@users.noreply.github.com>
2025-06-09 13:00:29 +0900

    fix: splash_mongbi.png 사이즈 2배율로 로드

--- lib/presentation/splash/splash_page.dart
+++ lib/presentation/splash/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
@@ -8,6 +9,7 @@ class SplashPage extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
+          // 배경 그라데이션
           Container(
             decoration: const BoxDecoration(
               gradient: LinearGradient(
@@ -28,6 +30,42 @@ class SplashPage extends StatelessWidget {
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
+          ),
+
+          // 콘텐츠 배치
+          SafeArea(
+            child: Column(
+              children: [
+                const SizedBox(height: 40),
+                Column(
+                  children: const [
+                    Text(
+                      '안녕, 난 몽비!',
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      '꿈을 먹는 도깨비다몽',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ],
+                ),
+
+                const Spacer(),
+                Center(
+                  child: Image.asset(
+                    'assets/images/splash_mongbi.png',
+                    width: 312,
+                    height: 312,
+                  ),
+                ),
+                const Spacer(flex: 2),
+              ],
+            ),
           ),
         ],
       ),
