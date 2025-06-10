6b1cffcf6e5868ca5df9f94f86bf2b9a03af4961
Meezzi <alswl-3016@naver.com>
2025-06-10 14:19:53 +0900

    feat: DreamWriteViewModel과 DreamWriteState를 위한 Provider 추가

--- lib/providers/dream_provider.dart
+++ lib/providers/dream_provider.dart
@@ -3,6 +3,8 @@ import 'package:mongbi_app/data/data_sources/dream_data_source.dart';
 import 'package:mongbi_app/data/data_sources/remote_dream_data_source.dart';
 import 'package:mongbi_app/data/repositories/remote_dream_repository.dart';
 import 'package:mongbi_app/domain/repositories/dream_repository.dart';
+import 'package:mongbi_app/presentation/dream/models/dream_write_state.dart';
+import 'package:mongbi_app/presentation/dream/view_models/dream_write_view_model.dart';
 import 'package:mongbi_app/providers/core_providers.dart';
 
 final _dreamDataSourceProvider = Provider<DreamDataSource>(
@@ -12,3 +14,8 @@ final _dreamDataSourceProvider = Provider<DreamDataSource>(
 final _dreamRepositoryProvider = Provider<DreamRepository>(
   (ref) => RemoteDreamRepository(ref.read(_dreamDataSourceProvider)),
 );
+
+final dreamWriteViewModelProvider =
+    StateNotifierProvider<DreamWriteViewModel, DreamWriteState>(
+      (ref) => DreamWriteViewModel(),
+    );
