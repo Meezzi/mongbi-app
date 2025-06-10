0deb1e5bebad23be5415f2feb496422e086eef0c
MoonHo <75512752+monho@users.noreply.github.com>
2025-06-10 12:40:34 +0900

    feat: 구글 로그인 로직 구현

--- lib/domain/entities/user.dart
+++ lib/domain/entities/user.dart
@@ -1,7 +1,8 @@
+// domain/entities/user.dart
 class User {
   final int userIdx;
   final String userName;
-  final String userNickname;
+  final String? userNickname;
   final String userType;
   final String userSocialType;
   final String userSocialId;
@@ -10,10 +11,10 @@ class User {
   final DateTime? userLastLoginDate;
   final String userIdState;
 
-  User({
+  const User({
     required this.userIdx,
     required this.userName,
-    required this.userNickname,
+    this.userNickname,
     required this.userType,
     required this.userSocialType,
     required this.userSocialId,
@@ -22,22 +23,4 @@ class User {
     this.userLastLoginDate,
     required this.userIdState,
   });
-
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-      userIdx: json['USER_IDX'],
-      userName: json['USER_NAME'],
-      userNickname: json['USER_NICKNAME'],
-      userType: json['USER_TYPE'],
-      userSocialType: json['USER_SOCIAL_TYPE'],
-      userSocialId: json['USER_SOCIAL_ID'],
-      userSocialUuid: json['USER_SOCIAL_UUID'],
-      userRegDate: DateTime.parse(json['USER_REG_DATE']),
-      userLastLoginDate:
-          json['USER_LAST_LOGIN_DATE'] != null
-              ? DateTime.parse(json['USER_LAST_LOGIN_DATE'])
-              : null,
-      userIdState: json['USER_ID_STATE'],
-    );
-  }
 }
