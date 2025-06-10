49442ebd4414c350af0cfdcafb71540e06c7d666
Meezzi <alswl-3016@naver.com>
2025-06-10 17:01:10 +0900

    refactor: API 변경에 따른 DreamDto 필드 및 메서드 이름 변경

--- lib/data/dtos/dream_dto.dart
+++ lib/data/dtos/dream_dto.dart
@@ -2,83 +2,83 @@ import 'package:mongbi_app/domain/entities/dream.dart';
 
 class DreamDto {
   DreamDto({
-    required this.id,
-    required this.createdAt,
-    required this.uid,
-    required this.challengeId,
-    required this.content,
-    required this.score,
+    required this.dreamIdx,
+    required this.dreamRegDate,
+    required this.userIdx,
+    required this.challengeIdx,
+    required this.dreamContent,
+    required this.dreamScore,
     required this.dreamKeywords,
     required this.dreamInterpretation,
     required this.psychologicalStateInterpretation,
     required this.psychologicalStateKeywords,
     required this.mongbiComment,
-    required this.emotionCategory,
+    required this.dreamCategory,
   });
 
-  final int id;
-  final DateTime createdAt;
-  final int uid;
-  final int challengeId;
-  final String content;
-  final int score;
+  final int dreamIdx;
+  final DateTime dreamRegDate;
+  final int userIdx;
+  final int challengeIdx;
+  final String dreamContent;
+  final int dreamScore;
   final List<String> dreamKeywords;
   final String dreamInterpretation;
   final String psychologicalStateInterpretation;
   final List<String> psychologicalStateKeywords;
   final String mongbiComment;
-  final String emotionCategory;
+  final String dreamCategory;
 
-  DreamDto.fromJson(Map<String, dynamic> json)
-    : id = json['DREAM_IDX'],
-      createdAt = json['DREAM_REG_DATE'],
-      uid = json['USER_IDX'],
-      challengeId = json['CHALLENGE_ID'],
-      content = json['DREAM_CONTENT'],
-      score = json['DREAM_SCORE'],
-      dreamKeywords =
-          (json['DREAM_KEYWORDS'] as List<dynamic>)
-              .map((e) => e as String)
-              .toList(),
-      dreamInterpretation = json['DREAM_INTERPRETATION'],
-      psychologicalStateInterpretation =
-          json['PSYCHOLOGICAL_STATELNTERPRETATION'],
-      psychologicalStateKeywords =
-          (json['PSYCHOLOGICALSTATE_KEYWORDS'] as List<dynamic>)
-              .map((e) => e as String)
-              .toList(),
-      mongbiComment = json['MONGBI_COMMENT'],
-      emotionCategory = json['EMOTION_CATEGORY'];
+  factory DreamDto.fromJson(Map<String, dynamic> json) {
+    return DreamDto(
+      dreamIdx: json['DREAM_IDX'],
+      dreamRegDate: DateTime.parse(json['DREAM_REG_DATE']),
+      userIdx: json['USER_IDX'],
+      challengeIdx: json['CHALLENGE_IDX'],
+      dreamContent: json['DREAM_CONTENT'],
+      dreamScore: json['DREAM_SCORE'],
+      dreamKeywords: (json['DREAM_KEYWORDS'] as List<dynamic>)
+          .map((e) => e as String)
+          .toList(),
+      dreamInterpretation: json['DREAM_INTERPRETATION'],
+      psychologicalStateInterpretation: json['PSYCHOLOGICAL_STATE_INTERPRETATION'],
+      psychologicalStateKeywords: (json['PSYCHOLOGICALSTATE_KEYWORDS'] as List<dynamic>)
+          .map((e) => e as String)
+          .toList(),
+      mongbiComment: json['MONGBI_COMMENT'],
+      dreamCategory: json['DREAM_CATEGORY'],
+    );
+  }
 
   Map<String, dynamic> toJson() => {
-    'DREAM_IDX': id,
-    'DREAM_REG_DATE': createdAt,
-    'USER_IDX': uid,
-    'CHALLENGE_ID': challengeId,
-    'DREAM_CONTENT': content,
-    'DREAM_SCORE': score,
-    'DREAM_KEYWORDS': dreamKeywords,
-    'DREAM_INTERPRETATION': dreamInterpretation,
-    'PSYCHOLOGICAL_STATELNTERPRETATION': psychologicalStateInterpretation,
-    'PSYCHOLOGICALSTATE_KEYWORDS': psychologicalStateKeywords,
-    'MONGBI_COMMENT': mongbiComment,
-    'EMOTION_CATEGORY': emotionCategory,
-  };
+        'DREAM_IDX': dreamIdx,
+        'DREAM_REG_DATE': dreamRegDate.toIso8601String(),
+        'USER_IDX': userIdx,
+        'CHALLENGE_IDX': challengeIdx,
+        'DREAM_CONTENT': dreamContent,
+        'DREAM_SCORE': dreamScore,
+        'DREAM_KEYWORDS': dreamKeywords,
+        'DREAM_INTERPRETATION': dreamInterpretation,
+        'PSYCHOLOGICAL_STATE_INTERPRETATION': psychologicalStateInterpretation,
+        'PSYCHOLOGICALSTATE_KEYWORDS': psychologicalStateKeywords,
+        'MONGBI_COMMENT': mongbiComment,
+        'DREAM_CATEGORY': dreamCategory,
+      };
 
   factory DreamDto.fromEntity(Dream dream) {
     return DreamDto(
-      id: dream.id ?? 0,
-      createdAt: dream.createdAt,
-      uid: dream.uid,
-      challengeId: dream.challengeId,
-      content: dream.content,
-      score: dream.score,
+      dreamIdx: dream.id ?? 0,
+      dreamRegDate: dream.createdAt,
+      userIdx: dream.uid,
+      challengeIdx: dream.challengeId,
+      dreamContent: dream.content,
+      dreamScore: dream.score,
       dreamKeywords: dream.dreamKeywords,
       dreamInterpretation: dream.dreamInterpretation,
       psychologicalStateInterpretation: dream.psychologicalStateInterpretation,
       psychologicalStateKeywords: dream.psychologicalStateKeywords,
       mongbiComment: dream.mongbiComment,
-      emotionCategory: dream.emotionCategory,
+      dreamCategory: dream.dreamCategory,
     );
   }
 }
