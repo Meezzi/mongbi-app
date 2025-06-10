446a5201d302b8232b055b2d82ea2523f0c3063d
Meezzi <alswl-3016@naver.com>
2025-06-10 14:17:38 +0900

    feat: 꿈 작성 화면에서 상태를 관리하는 DreamWriteViewModel 구현

---
+++ lib/presentation/dream/view_models/dream_write_view_model.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mongbi_app/presentation/dream/models/dream_write_state.dart';
+
+class DreamWriteViewModel extends StateNotifier<DreamWriteState> {
+  DreamWriteViewModel() : super(DreamWriteState());
+
+  void setDreamContent(String content) {
+    state = state.copyWith(dreamContent: content);
+  }
+
+  void setSelectedIndex(int index) {
+    state = state.copyWith(selectedIndex: index);
+  }
+
+  void setFocused(bool focused) {
+    state = state.copyWith(isFocused: focused);
+  }
+
+  bool submitDream() {
+    // 텍스트 필드 내용이 10글자 미만인 경우
+    if (state.dreamContent.trim().length < 10) {
+      return false;
+    }
+    // 감정이 선택되지 않은 경우
+    if (state.selectedIndex == -1) {
+      return false;
+    }
+
+    // TODO: 꿈 해석 로직
+
+    return true;
+  }
+}
+
