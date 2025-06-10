3fcb9e44d00a2abb1921196166b7b1fb359484a9
Meezzi <alswl-3016@naver.com>
2025-06-10 17:05:38 +0900

    fix: analyzeDream 메서드의 반환 타입을 String에서 Dream으로 변경

--- lib/domain/repositories/dream_repository.dart
+++ lib/domain/repositories/dream_repository.dart
@@ -3,5 +3,5 @@ import 'package:mongbi_app/domain/entities/dream.dart';
 abstract interface class DreamRepository {
   Future<bool> saveDream(Dream dream);
 
-  Future<String> analyzeDream(String dreamContent, int dreamScore);
+  Future<Dream> analyzeDream(String dreamContent, int dreamScore);
 }
