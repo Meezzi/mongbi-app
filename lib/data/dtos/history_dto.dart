ffa2ab23eb1a683087d7d4b322484c24190821a2
leehyunjin <skyhyunjinlee@gmail.com>
2025-06-11 00:52:59 +0900

    refactor: history dto, entity 수정

--- lib/data/dtos/history_dto.dart
+++ lib/data/dtos/history_dto.dart
@@ -2,20 +2,20 @@ class HistoryDto {
   HistoryDto({
     required this.dreamContent,
     required this.dreamScore,
-    required this.dreamTag,
     required this.dreamKeywords,
     required this.dreamInterpretation,
     required this.psychologicalStateInterpretation,
     required this.psychologicalStateKeywords,
     required this.mongbiComment,
-    required this.emotionCategory,
     required this.dreamRegDate,
+    required this.dreamIdx,
+    required this.userIdx,
+    required this.challengeIdx,
   });
 
   factory HistoryDto.fromJson(Map<String, dynamic> json) => HistoryDto(
     dreamContent: json['DREAM_CONTENT'],
     dreamScore: json['DREAM_SCORE'],
-    dreamTag: json['DREAM_TAG'],
     dreamKeywords: List<String>.from(json['DREAM_KEYWORDS'].map((x) => x)),
     dreamInterpretation: json['DREAM_INTERPRETATION'],
     psychologicalStateInterpretation:
@@ -24,25 +24,27 @@ class HistoryDto {
       json['PSYCHOLOGICALSTATE_KEYWORDS'].map((x) => x),
     ),
     mongbiComment: json['MONGBI_COMMENT'],
-    emotionCategory: json['EMOTION_CATEGORY'],
-    dreamRegDate: json['DREAM_REG_DATE'],
+    dreamRegDate: DateTime.parse(json['DREAM_REG_DATE']),
+    dreamIdx: json['DREAM_IDX'],
+    userIdx: json['USER_IDX'],
+    challengeIdx: json['CHALLENGE_IDX'],
   );
 
   final String dreamContent;
   final int dreamScore;
-  final String dreamTag;
   final List<String> dreamKeywords;
   final String dreamInterpretation;
   final String psychologicalStateInterpretation;
   final List<String> psychologicalStateKeywords;
   final String mongbiComment;
-  final String emotionCategory;
-  final String dreamRegDate;
+  final DateTime dreamRegDate;
+  final int dreamIdx;
+  final int userIdx;
+  final int challengeIdx;
 
   Map<String, dynamic> toJson() => {
     'DREAM_CONTENT': dreamContent,
     'DREAM_SCORE': dreamScore,
-    'DREAM_TAG': dreamTag,
     'DREAM_KEYWORDS': List<dynamic>.from(dreamKeywords.map((x) => x)),
     'DREAM_INTERPRETATION': dreamInterpretation,
     'PSYCHOLOGICAL_STATELNTERPRETATION': psychologicalStateInterpretation,
@@ -50,7 +52,9 @@ class HistoryDto {
       psychologicalStateKeywords.map((x) => x),
     ),
     'MONGBI_COMMENT': mongbiComment,
-    'EMOTION_CATEGORY': emotionCategory,
-    'DREAM_REG_DATE': dreamRegDate,
+    'DREAM_REG_DATE': dreamRegDate.toIso8601String(),
+    'DREAM_IDX': dreamIdx,
+    'USER_IDX': userIdx,
+    'CHALLENGE_IDX': challengeIdx,
   };
 }
--- lib/domain/entities/history.dart
+++ lib/domain/entities/history.dart
@@ -2,24 +2,20 @@ class History {
   History({
     required this.dreamContent,
     required this.dreamScore,
-    required this.dreamTag,
     required this.dreamKeywords,
     required this.dreamInterpretation,
     required this.psychologicalStateInterpretation,
     required this.psychologicalStateKeywords,
     required this.mongbiComment,
-    required this.emotionCategory,
     required this.dreamRegDate,
   });
 
   final String dreamContent;
   final int dreamScore;
-  final String dreamTag;
   final List<String> dreamKeywords;
   final String dreamInterpretation;
   final String psychologicalStateInterpretation;
   final List<String> psychologicalStateKeywords;
   final String mongbiComment;
-  final String emotionCategory;
-  final String dreamRegDate;
+  final DateTime dreamRegDate;
 }
