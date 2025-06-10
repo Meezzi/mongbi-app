3539456a4b1466ee9f94418b88d6a1d8117792a0
leehyunjin <skyhyunjinlee@gmail.com>
2025-06-11 00:57:22 +0900

    refactor: 데이터 소스와 뷰모델 변경

---
+++ lib/data/repositories/history_repository_impl.dart
@@ -0,0 +1,29 @@
+import 'package:mongbi_app/data/data_sources/history_data_source.dart';
+import 'package:mongbi_app/domain/entities/history.dart';
+import 'package:mongbi_app/domain/repositories/history_repository.dart';
+
+class HistoryRepositoryImpl implements HistoryRepository {
+  HistoryRepositoryImpl(this._dataSource);
+
+  final HistoryDataSource _dataSource;
+
+  @override
+  Future<List<History>> feachUserDreamsHistory() async {
+    final historyDtoList = await _dataSource.feachUserDreamsHistory();
+    return historyDtoList
+        .map(
+          (e) => History(
+            dreamContent: e.dreamContent,
+            dreamScore: e.dreamScore,
+            dreamKeywords: e.dreamKeywords,
+            dreamInterpretation: e.dreamInterpretation,
+            psychologicalStateInterpretation:
+                e.psychologicalStateInterpretation,
+            psychologicalStateKeywords: e.psychologicalStateKeywords,
+            mongbiComment: e.mongbiComment,
+            dreamRegDate: e.dreamRegDate,
+          ),
+        )
+        .toList();
+  }
+}
--- lib/presentation/history/view_models/calendar_view_model.dart
+++ lib/presentation/history/view_models/calendar_view_model.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mongbi_app/presentation/history/models/calendar_model.dart';
+import 'package:mongbi_app/providers/history_provider.dart';
 
 class CalendarViewModel extends AutoDisposeNotifier<CalendarModel> {
   @override
@@ -16,15 +17,17 @@ class CalendarViewModel extends AutoDisposeNotifier<CalendarModel> {
   }
 
   void onDaySelected(DateTime selectedDay, DateTime focusedDay) {
-    state = state.copyWith(selectedDay: selectedDay, focusedDay: focusedDay);
+    final historyVm = ref.read(historyViewModelProvider.notifier);
+    final searchedHistory = historyVm.searchDateTime(selectedDay);
+
+    state = state.copyWith(
+      selectedDay: selectedDay,
+      focusedDay: focusedDay,
+      searchedHistory: searchedHistory,
+    );
   }
 
   void onPageChanged(DateTime focusedDay) {
     state = state.copyWith(focusedDay: focusedDay);
   }
 }
-
-final calendarViewModelProvider =
-    NotifierProvider.autoDispose<CalendarViewModel, CalendarModel>(() {
-      return CalendarViewModel();
-    });
