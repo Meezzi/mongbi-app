abb667aa812888c8efb244364d95318dd07a8d38
Meezzi <alswl-3016@naver.com>
2025-06-11 15:33:14 +0900

    feat: DreamInterpretationState 모델 추가

---
+++ lib/presentation/dream/models/dream_interpretation_state.dart
@@ -0,0 +1,19 @@
+class DreamInterpretationState {
+  DreamInterpretationState({
+    this.dreamSubTitle = '',
+    this.dreamInterpretation = '',
+    this.dreamKeywords = const [],
+    this.psychologicalSubTitle = '',
+    this.psychologicalStateInterpretation = '',
+    this.psychologicalStateKeywords = const [],
+    this.mongbiComment = '',
+  });
+
+  final String dreamSubTitle;
+  final String dreamInterpretation;
+  final List<String> dreamKeywords;
+  final String psychologicalSubTitle;
+  final String psychologicalStateInterpretation;
+  final List<String> psychologicalStateKeywords;
+  final String mongbiComment;
+}
