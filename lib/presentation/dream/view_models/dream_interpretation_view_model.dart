89dde54680d16c1ba364da866dd51f546f928cec
Meezzi <alswl-3016@naver.com>
2025-06-11 19:47:41 +0900

    feat: saveDream 메서드 추가 및 dreamCategory 필드 사용

--- lib/presentation/dream/view_models/dream_interpretation_view_model.dart
+++ lib/presentation/dream/view_models/dream_interpretation_view_model.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mongbi_app/domain/entities/dream.dart';
 import 'package:mongbi_app/presentation/dream/models/dream_interpretation_state.dart';
+import 'package:mongbi_app/providers/dream_provider.dart';
 
 class DreamInterpretationViewModel extends Notifier<DreamInterpretationState> {
   @override
@@ -18,5 +19,27 @@ class DreamInterpretationViewModel extends Notifier<DreamInterpretationState> {
       psychologicalStateKeywords: dream.psychologicalStateKeywords,
       mongbiComment: dream.mongbiComment,
     );
+  }
+
+  Future<bool> saveDream() async {
+    final dreamWriteState = ref.read(dreamWriteViewModelProvider);
+    final dreamEntity = Dream(
+      createdAt: DateTime.now(),
+      uid: 4, // TODO: 실제 로그인 사용자 ID로 교체
+      challengeId: 0, // TODO: 실제 챌린지 ID로 교체
+      content: dreamWriteState.dreamContent,
+      score: dreamWriteState.selectedIndex,
+      dreamKeywords: state.dreamKeywords,
+      dreamSubTitle: state.dreamSubTitle,
+      dreamInterpretation: state.dreamInterpretation,
+      psychologicalSubTitle: state.psychologicalSubTitle,
+      psychologicalStateInterpretation: state.psychologicalStateInterpretation,
+      psychologicalStateKeywords: state.psychologicalStateKeywords,
+      mongbiComment: state.mongbiComment,
+      dreamCategory: state.dreamCategory,
+    );
+
+    final repository = ref.read(dreamRepositoryProvider);
+    return await repository.saveDream(dreamEntity);
   }
 }
