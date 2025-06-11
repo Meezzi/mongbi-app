0a4530008f578d6eaadc0a4dfef3008cc936fed8
Meezzi <alswl-3016@naver.com>
2025-06-11 16:13:20 +0900

    refactor: main.dart에서 사용하지 않는 import문 제거

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:mongbi_app/core/router.dart';
-import 'package:mongbi_app/presentation/auth/social_login_page.dart';
-import 'package:mongbi_app/presentation/dream/dream_interpretation_page.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
