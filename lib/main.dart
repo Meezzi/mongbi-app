199cdfddf9b69273243f55d7d3c3619379f646b8
MoonHo <75512752+monho@users.noreply.github.com>
2025-06-12 21:32:51 +0900

    feat: 기본 프로필 설정 UI 구현

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:mongbi_app/core/router.dart';
+import 'package:mongbi_app/presentation/auth/social_login_page.dart';
+import 'package:mongbi_app/presentation/profile/profile_setting_page.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
