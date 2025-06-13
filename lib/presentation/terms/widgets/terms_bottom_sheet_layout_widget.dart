6a46d202bc3f402058bd0af0d8315b2dab3e2b65
MoonHo <75512752+monho@users.noreply.github.com>
2025-06-13 16:06:08 +0900

    fix: 에러 수정

--- lib/presentation/terms/widgets/terms_bottom_sheet_layout_widget.dart
+++ lib/presentation/terms/widgets/terms_bottom_sheet_layout_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:mongbi_app/data/dtos/terms_aggrement_dto.dart';
 import 'package:mongbi_app/domain/entities/terms.dart';
 import 'package:mongbi_app/presentation/terms/widgets/terms_button_widget.dart';
--- lib/providers/core_providers.dart
+++ lib/providers/core_providers.dart
@@ -2,13 +2,11 @@ import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final dioProvider = Provider<Dio>(
-  (ref) => Dio(BaseOptions(baseUrl: dotenv.env['MONGBI_BASE_URL']!)),
-);
-
 final admindioProvider = Provider<Dio>(
   (ref) => Dio(BaseOptions(baseUrl: dotenv.env['ADMIN_MONGBI_BASE_URL']!)),
 );
+
+final dioProvider = Provider<Dio>(
   (ref) => Dio(
     BaseOptions(
       baseUrl: dotenv.env['MONGBI_BASE_URL']!,
@@ -19,4 +17,3 @@ final admindioProvider = Provider<Dio>(
     ),
   ),
 );
-
