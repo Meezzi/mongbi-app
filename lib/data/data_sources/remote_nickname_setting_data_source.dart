dd4fe3e0e62dc1f1d7376fc8f3b138aba03d78de
MoonHo <75512752+monho@users.noreply.github.com>
2025-06-13 12:25:19 +0900

    fit: 데이터소스,레포지토리,유스케이스 구현

--- lib/data/data_sources/remote_nickname_setting_data_source.dart
+++ lib/data/data_sources/remote_nickname_setting_data_source.dart
@@ -1,28 +1,41 @@
 import 'package:dio/dio.dart';
-import 'package:mongbi_app/data/data_sources/nickname_setting_data_source.dart';
 import 'package:mongbi_app/data/dtos/user_dto.dart';
+import 'package:mongbi_app/domain/entities/user.dart';
 
-class NickNameSettingDataSourceImpl implements NickNameSettingDataSource {
-  NickNameSettingDataSourceImpl(this.dio);
+abstract class UserRemoteDataSource {
+  Future<User> updateNickname({
+    required int userId,
+    required String nickname,
+  });
+}
+
+class UserRemoteDataSourceImpl implements UserRemoteDataSource {
+
+  UserRemoteDataSourceImpl(this.dio);
   final Dio dio;
 
   @override
-  Future<UserDto> nickNameSetting({
+  Future<User> updateNickname({
     required int userId,
     required String nickname,
   }) async {
-    final response = await dio.put(
-      '/users/$userId/nickname',
-      data: {'nickname': nickname},
-      options: Options(
-        validateStatus: (_) => true, // ✅ 200이 아니어도 print는 실행됨
-      ),
-    );
-    
-    if (response.statusCode == 200 && response.data['user'] != null) {
-      return UserDto.fromJson(response.data['user']);
-    } else {
-      throw Exception('닉네임 수정 실패');
+    try {
+      final response = await dio.patch(
+        '/users/$userId/nickname',
+        data: {'nickname': nickname},
+      );
+
+      if (response.statusCode == 200 && response.data['user'] != null) {
+        final userDto = UserDto.fromJson(response.data['user']);
+        return userDto.toEntity();
+      } else {
+        throw Exception(response.data['message'] ?? '닉네임 변경 실패');
+      }
+    } on DioException catch (e) {
+      final errorMessage = e.response?.data['message'] ?? '네트워크 오류';
+      throw Exception(errorMessage);
+    } catch (e) {
+      rethrow;
     }
   }
 }
