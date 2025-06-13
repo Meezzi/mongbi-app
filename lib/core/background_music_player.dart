f7fe769e98f837947e81b00dee6a57bbd857f9e1
Meezzi <alswl-3016@naver.com>
2025-06-13 14:16:20 +0900

    feat: BackgroundMusicPlayer에 재생 재개 기능 및 일시정지 상태 관리 추가

--- lib/core/background_music_player.dart
+++ lib/core/background_music_player.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:audioplayers/audioplayers.dart';
 
 class BackgroundMusicPlayer {
@@ -6,9 +7,26 @@ class BackgroundMusicPlayer {
   }
 
   final AudioPlayer _player;
+  bool _isPaused = false;
 
   void playBgm() async {
     await _player.setVolume(1.0);
     await _player.play(AssetSource('audio/background_music.mp3'));
+    _isPaused = false;
+  }
+
+  void resumeBgm() async {
+    if (_isPaused) {
+      try {
+        await _player.resume();
+        _isPaused = false;
+      } catch (e) {
+        playBgm();
+      }
+    }
+  }
+
+  void dispose() {
+    _player.dispose();
   }
 }
