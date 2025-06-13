d9aa70a15d1aa4ccd7b805b8d052b3d31fd25ac0
MoonHo <75512752+monho@users.noreply.github.com>
2025-06-13 11:07:54 +0900

    fix: 사용하지 않는 구글 로그인 관련 파일 제거 및 네이밍 컨벤션 수정

--- lib/data/data_sources/kakao_auth_data_source.dart
+++ lib/data/data_sources/kakao_auth_data_source.dart
@@ -1,5 +1,5 @@
 import 'package:mongbi_app/data/dtos/login_response_dto.dart';
 
 abstract class AuthDataSource {
-  Future<LoginResponseDto> KakaoLogin(String idToken);
+  Future<LoginResponseDto> kakaoLogin(String idToken);
 }
