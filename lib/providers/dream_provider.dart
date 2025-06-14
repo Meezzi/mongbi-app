67818d36cb6d674471b55d48f550765560437d96
Meezzi <alswl-3016@naver.com>
2025-06-14 20:03:42 +0900

    refactor: 이전 UseCase 제거 후, analyzeAndSaveDreamUseCaseProvider 생성

--- lib/providers/dream_provider.dart
+++ lib/providers/dream_provider.dart
@@ -6,8 +6,7 @@ import 'package:mongbi_app/data/data_sources/remote_dream_analysis_data_source.d
 import 'package:mongbi_app/data/data_sources/remote_dream_data_source.dart';
 import 'package:mongbi_app/data/repositories/remote_dream_repository.dart';
 import 'package:mongbi_app/domain/repositories/dream_repository.dart';
-import 'package:mongbi_app/domain/use_cases/analyze_dream_use_case.dart';
-import 'package:mongbi_app/domain/use_cases/dream_save_use_case.dart';
+import 'package:mongbi_app/domain/use_cases/analyze_and_save_dream_use_case.dart';
 import 'package:mongbi_app/presentation/dream/models/dream_interpretation_state.dart';
 import 'package:mongbi_app/presentation/dream/models/dream_write_state.dart';
 import 'package:mongbi_app/presentation/dream/view_models/dream_interpretation_view_model.dart';
@@ -34,12 +33,8 @@ final _dreamRepositoryProvider = Provider<DreamRepository>(
   ),
 );
 
-final analyzeDreamUseCaseProvider = Provider<AnalyzeDreamUseCase>(
-  (ref) => AnalyzeDreamUseCase(ref.read(_dreamRepositoryProvider)),
-);
-
-final saveDreamUseCaseProvider = Provider<DreamSaveUseCase>(
-  (ref) => DreamSaveUseCase(ref.read(_dreamRepositoryProvider)),
+final analyzeAndSaveDreamUseCaseProvider = Provider<AnalyzeAndSaveDreamUseCase>(
+  (ref) => AnalyzeAndSaveDreamUseCase(ref.read(_dreamRepositoryProvider)),
 );
 
 final dreamWriteViewModelProvider =
