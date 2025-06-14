703e61ec44a746c56fcdd55cb6dab588f991ebf7
Meezzi <alswl-3016@naver.com>
2025-06-14 20:21:43 +0900

    refactor: DreamInterpretationViewModel에서는 꿈 해석만 표시하고 저장 로직 제거

--- lib/presentation/dream/view_models/dream_interpretation_view_model.dart
+++ lib/presentation/dream/view_models/dream_interpretation_view_model.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mongbi_app/domain/entities/dream.dart';
 import 'package:mongbi_app/presentation/dream/models/dream_interpretation_state.dart';
-import 'package:mongbi_app/providers/dream_provider.dart';
 
 class DreamInterpretationViewModel extends Notifier<DreamInterpretationState> {
   @override
@@ -19,26 +18,5 @@ class DreamInterpretationViewModel extends Notifier<DreamInterpretationState> {
       psychologicalStateKeywords: dream.psychologicalStateKeywords,
       mongbiComment: dream.mongbiComment,
     );
-  }
-
-  Future<bool> saveDream() async {
-    final dreamWriteState = ref.read(dreamWriteViewModelProvider);
-    final dreamEntity = Dream(
-      createdAt: DateTime.now(),
-      uid: 4, // TODO: 실제 로그인 사용자 ID로 교체
-      challengeId: 0, // TODO: 실제 챌린지 ID로 교체
-      content: dreamWriteState.dreamContent,
-      score: dreamWriteState.selectedIndex,
-      dreamKeywords: state.dreamKeywords,
-      dreamSubTitle: state.dreamSubTitle,
-      dreamInterpretation: state.dreamInterpretation,
-      psychologicalSubTitle: state.psychologicalSubTitle,
-      psychologicalStateInterpretation: state.psychologicalStateInterpretation,
-      psychologicalStateKeywords: state.psychologicalStateKeywords,
-      mongbiComment: state.mongbiComment,
-      dreamCategory: state.dreamCategory,
-    );
-
-    return await ref.read(saveDreamUseCaseProvider).saveDream(dreamEntity);
   }
 }
