bcf6ec1aa9830cc39fce5d49bcfa28d76ccdf9b2
Meezzi <alswl-3016@naver.com>
2025-06-14 15:34:43 +0900

    feat: alarmSettingProvider 생성

---
+++ lib/providers/alarm_setting_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mongbi_app/presentation/setting/models/alarm_setting_state.dart';
+import 'package:mongbi_app/presentation/setting/view_models/alarm_setting_view_model.dart';
+
+final alarmSettingProvider =
+    NotifierProvider<AlarmSettingViewModel, AlarmSettingState>(
+      () => AlarmSettingViewModel(),
+    );
