fc712dcace4fee22f38e5e11b6afb05c26fa8d97
Meezzi <alswl-3016@naver.com>
2025-06-14 16:04:59 +0900

    feat: bgmProvider 생성

--- lib/providers/alarm_setting_provider.dart
+++ lib/providers/alarm_setting_provider.dart
@@ -1,8 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mongbi_app/presentation/setting/models/alarm_setting_state.dart';
+import 'package:mongbi_app/presentation/setting/models/bgm_state.dart';
 import 'package:mongbi_app/presentation/setting/view_models/alarm_setting_view_model.dart';
 
 final alarmSettingProvider =
     NotifierProvider<AlarmSettingViewModel, AlarmSettingState>(
       () => AlarmSettingViewModel(),
     );
+
+final bgmProvider = NotifierProvider<BgmState, bool>(() => BgmState());
