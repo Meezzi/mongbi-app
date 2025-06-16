9709ffd9b1a81e501d9761a249afcf2913b0d926
MoonHo <75512752+monho@users.noreply.github.com>
2025-06-16 19:10:39 +0900

    feat: 기본 커스텀 위젯 구성

---
+++ lib/presentation/remind/widgets/remind_time_setting_button_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:mongbi_app/core/font.dart';
+
+class RemindTimeSettingButtonWidget extends StatelessWidget {
+  const RemindTimeSettingButtonWidget({
+    super.key,
+    required this.onTap,
+  });
+
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        width: double.infinity,
+        height: 56,
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 18),
+          decoration: BoxDecoration(
+            color: const Color(0xFF8C2EFF),
+            borderRadius: BorderRadius.circular(40),
+          ),
+          child: Text(
+            '알겠어',
+            textAlign: TextAlign.center,
+            style: Font.title18.copyWith(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
