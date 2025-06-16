dcfc785355fc80db376a11671e85a9ec2b32ac67
MoonHo <75512752+monho@users.noreply.github.com>
2025-06-16 19:55:41 +0900

    feat: OS별 권한 요청 기능 구현

---
+++ lib/presentation/remind/view_model/remind_time_setting_view_model.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> requestNotificationPermission() async {
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+        IOSFlutterLocalNotificationsPlugin
+      >()
+      ?.requestPermissions(alert: true, badge: true, sound: true);
+
+  if (await Permission.notification.isDenied) {
+    await Permission.notification.request();
+  }
+}
