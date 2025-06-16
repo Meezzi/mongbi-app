dfe1229aa2e4dab440dd202f2514f0991ded48a1
MoonHo <75512752+monho@users.noreply.github.com>
2025-06-16 19:42:06 +0900

    feat: 리마인드 셋  페이지 구현 및 이미지 추가

---
+++ lib/presentation/remind/remind_time_setting_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mongbi_app/presentation/remind/widgets/remind_time_setting_button_widget.dart';
+import 'package:mongbi_app/presentation/remind/widgets/remind_time_setting_image_widget.dart';
+import 'package:mongbi_app/presentation/remind/widgets/remind_time_setting_text_widget.dart';
+
+class RemindTimeSettingPage extends StatelessWidget {
+  const RemindTimeSettingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 24, top: 16),
+          child: IconButton(
+            icon: SvgPicture.asset(
+              'assets/icons/back-arrow.svg',
+              width: 24,
+              height: 24,
+            ),
+            onPressed: () => context.go('/home'),
+            splashRadius: 20,
+          ),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.black,
+        toolbarHeight: 56,
+        titleSpacing: 0,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            children: [
+              const Spacer(),
+              const OnboardingText(
+                text: '몽비가 알림을 보내줄까몽?',
+                type: RemindTimeSettingTextType.title,
+              ),
+              const OnboardingText(
+                text: '매일 몽비와 함께 할 수 있도록',
+                type: RemindTimeSettingTextType.description,
+              ),
+              const OnboardingText(
+                text: '알림을 보내드릴게요.',
+                type: RemindTimeSettingTextType.description,
+              ),
+              const SizedBox(height: 48),
+              const RemindTimeSettingImageWidget(
+                assetPath: 'assets/images/remind_bell.webp',
+              ),
+              const Spacer(),
+              RemindTimeSettingButtonWidget(
+                onTap: () {
+                  context.go('/onbording_exit');
+                },
+              ),
+              const SizedBox(height: 32),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
