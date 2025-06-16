aeeff7d2b33cc95b1f0a4d45872f30a4be341a93
Meezzi <alswl-3016@naver.com>
2025-06-16 23:21:47 +0900

    refactor: 앱을 처음 실행했을 때 무조건 배경음악이 재생되는 것에서 배경음악 토글이 켜지면 배경음악 재생으로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:mongbi_app/core/router.dart';
 import 'package:mongbi_app/providers/background_music_provider.dart';
+import 'package:mongbi_app/providers/setting_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,7 +35,11 @@ class _MyAppState extends ConsumerState<MyApp> with WidgetsBindingObserver {
   void initState() {
     super.initState();
     WidgetsBinding.instance.addObserver(this);
-    ref.read(backgroundMusicProvider).playBgm();
+
+    final isBgmOn = ref.read(bgmProvider);
+    if (isBgmOn) {
+      ref.read(backgroundMusicProvider).playBgm();
+    }
   }
 
   @override
