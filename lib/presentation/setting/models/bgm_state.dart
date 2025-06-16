1e0740b132abfe07284f202791408c55fd4dc866
Meezzi <alswl-3016@naver.com>
2025-06-16 17:59:58 +0900

    refactor: shared_preferences를 사용하여 배경음악 재생/정지 여부를 저장

--- lib/presentation/setting/models/bgm_state.dart
+++ lib/presentation/setting/models/bgm_state.dart
@@ -1,19 +1,45 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mongbi_app/providers/background_music_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class BgmState extends Notifier<bool> {
+  static const _bgmKey = 'isBgmOn';
+
   @override
   bool build() {
+    _loadPersistedState();
     return true;
   }
 
+  void _loadPersistedState() async {
+    final prefs = await SharedPreferences.getInstance();
+    final saved = prefs.getBool(_bgmKey);
+    if (saved != null && saved != state) {
+      state = saved;
+    }
+  }
+
   Future<void> turnOn() async {
+    if (state) {
+      await ref.read(backgroundMusicProvider).playBgm();
+      return;
+    }
+
     state = true;
-    ref.read(backgroundMusicProvider).playBgm();
+    await _persistState(true);
+    await ref.read(backgroundMusicProvider).playBgm();
   }
 
   Future<void> turnOff() async {
+    if (!state) return;
+
     state = false;
+    await _persistState(false);
     await ref.read(backgroundMusicProvider).fadeOutAndPause();
+  }
+
+  Future<void> _persistState(bool value) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_bgmKey, value);
   }
 }
