44678b01a635448ea5b0ad9b188686e6840de24a
MoonHo <75512752+monho@users.noreply.github.com>
2025-06-16 17:29:41 +0900

    feat: 온보팅 exit페이지 커스텀 위젯 작업 완료

---
+++ lib/presentation/onboarding/widgets/onbording_exit_button_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:mongbi_app/core/font.dart';
+
+class OnbordingExitButtonWidget extends StatelessWidget {
+  const OnbordingExitButtonWidget({
+    super.key,
+    required this.onTap,
+  });
+
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        width: double.infinity,
+        height: 56,
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 18),
+          decoration: BoxDecoration(
+            color: const Color(0xFF8C2EFF),
+            borderRadius: BorderRadius.circular(40),
+          ),
+          child: Text(
+            '시작할게',
+            textAlign: TextAlign.center,
+            style: Font.title18.copyWith(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
