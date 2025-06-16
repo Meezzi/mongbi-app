bcad567550b111e8376e69e054f91f5495ac0450
MoonHo <75512752+monho@users.noreply.github.com>
2025-06-16 19:12:01 +0900

    fix: 텍스트 커스텀 위젯 수정

--- lib/presentation/remind/widgets/remind_time_setting_text_widget.dart
+++ lib/presentation/remind/widgets/remind_time_setting_text_widget.dart
@@ -1,26 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:mongbi_app/core/font.dart';
 
-class CustomText extends StatelessWidget {
-  const CustomText({
+enum RemindTimeSettingTextType { title, description }
+
+class OnboardingText extends StatelessWidget {
+  const OnboardingText({
     super.key,
     required this.text,
-    this.color,
-    this.align = TextAlign.start,
+    required this.type,
+    this.align = TextAlign.center,
   });
-
   final String text;
-  final Color? color;
+  final RemindTimeSettingTextType type;
   final TextAlign align;
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      textAlign: align,
-      style: Font.title20.copyWith(
-        color: color ?? const Color(0xFF1A181B),
-      ),
-    );
+    TextStyle style;
+
+    switch (type) {
+      case RemindTimeSettingTextType.title:
+        style = Font.title20.copyWith(color: Color(0xFF1A181B));
+        break;
+      case RemindTimeSettingTextType.description:
+        style = Font.body16.copyWith(color: Color(0xFF636363));
+        break;
+    }
+
+    return Text(text, style: style, textAlign: align);
   }
 }
