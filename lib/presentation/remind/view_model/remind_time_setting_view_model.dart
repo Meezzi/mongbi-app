b8b852a0a8ebabdd933b5a31f451f32c70b72854
MoonHo <75512752+monho@users.noreply.github.com>
2025-06-16 20:28:52 +0900

    feat: 디바이스별 권한요청 기능 구현

--- lib/presentation/remind/view_model/remind_time_setting_view_model.dart
+++ lib/presentation/remind/view_model/remind_time_setting_view_model.dart
@@ -4,14 +4,13 @@ import 'package:permission_handler/permission_handler.dart';
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
 
-Future<void> requestNotificationPermission() async {
+Future<bool> requestNotificationPermission() async {
   await flutterLocalNotificationsPlugin
       .resolvePlatformSpecificImplementation<
         IOSFlutterLocalNotificationsPlugin
       >()
       ?.requestPermissions(alert: true, badge: true, sound: true);
 
-  if (await Permission.notification.isDenied) {
-    await Permission.notification.request();
-  }
+  final status = await Permission.notification.request();
+  return status.isGranted;
 }
