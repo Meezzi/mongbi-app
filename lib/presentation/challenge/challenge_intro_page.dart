4f820b000141ac8ac9f9295678d96d40124c2138
Meezzi <alswl-3016@naver.com>
2025-06-17 20:00:26 +0900

    feat: 공통 위젯인 ActionButtonRow 재사용

--- lib/presentation/challenge/challenge_intro_page.dart
+++ lib/presentation/challenge/challenge_intro_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mongbi_app/presentation/challenge/widgets/challenge_intro_action_button.dart';
 import 'package:mongbi_app/presentation/challenge/widgets/challenge_intro_mongbi_message_view.dart';
+import 'package:mongbi_app/presentation/common/action_button_row.dart';
 
 class ChallengeIntroPage extends StatelessWidget {
   const ChallengeIntroPage({super.key});
@@ -15,32 +15,15 @@ class ChallengeIntroPage extends StatelessWidget {
             Expanded(child: MongbiMessageView()),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 24.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: ActionButton(
-                      onPressed: () {
-                        // TODO: 홈 화면으로 이동
-                      },
-                      backgroundColor: const Color(0xFFF4EAFF),
-                      textColor: const Color(0xFFB273FF),
-                      shadowAlpha: 10,
-                      text: '괜찮아',
-                    ),
-                  ),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: ActionButton(
-                      onPressed: () {
-                        // TODO: 챌린지 제안
-                      },
-                      backgroundColor: const Color(0xFF8C2EFF),
-                      textColor: Colors.white,
-                      shadowAlpha: 20,
-                      text: '선물이 뭐야?',
-                    ),
-                  ),
-                ],
+              child: ActionButtonRow(
+                leftText: '괜찮아',
+                rightText: '선물이 뭐야?',
+                onLeftPressed: () {
+                  // TODO: 홈 화면으로 이동
+                },
+                onRightPressed: () {
+                  // TODO: 챌린지 제안
+                },
               ),
             ),
             const SizedBox(height: 24),
