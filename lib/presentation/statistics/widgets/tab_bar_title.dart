37b76ea6dea7be7aca920d3047a7dd2d34ea8654
leehyunjin <skyhyunjinlee@gmail.com>
2025-06-17 22:25:55 +0900

    feat: sliver 구현

--- lib/presentation/statistics/widgets/tab_bar_title.dart
+++ lib/presentation/statistics/widgets/tab_bar_title.dart
@@ -4,10 +4,13 @@ import 'package:mongbi_app/core/font.dart';
 import 'package:mongbi_app/core/get_responsive_ratio_by_width.dart';
 import 'package:mongbi_app/providers/statistics_provider.dart';
 
-class TabBarTitle extends StatelessWidget {
+class TabBarTitle extends StatelessWidget implements PreferredSizeWidget {
   const TabBarTitle({super.key, required this.horizontalPadding});
 
   final double horizontalPadding;
+
+  @override
+  Size get preferredSize => const Size.fromHeight(70); // 원하는 높이로 조절
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +22,7 @@ class TabBarTitle extends StatelessWidget {
       child: Container(
         padding: EdgeInsets.all(getResponsiveRatioByWidth(context, 4)),
         decoration: BoxDecoration(
-          color: Color(0xFFF4EAFF),
+          color: const Color(0xFFF4EAFF),
           borderRadius: BorderRadius.circular(999),
         ),
         height: getResponsiveRatioByWidth(context, 48),
@@ -35,7 +38,7 @@ class TabBarTitle extends StatelessWidget {
                   statisticsVm.fetchYearStatistics();
                 }
               },
-              tabs: [Tab(child: Text('월간')), Tab(child: Text('연간'))],
+              tabs: const [Tab(child: Text('월간')), Tab(child: Text('연간'))],
               labelPadding: EdgeInsets.zero,
               indicatorPadding: EdgeInsets.zero,
               indicator: BoxDecoration(
@@ -43,16 +46,16 @@ class TabBarTitle extends StatelessWidget {
                 borderRadius: BorderRadius.circular(999),
               ),
               indicatorSize: TabBarIndicatorSize.tab,
-              labelColor: Color(0xFF3B136B),
+              labelColor: const Color(0xFF3B136B),
               labelStyle: Font.title14.copyWith(
                 fontSize: getResponsiveRatioByWidth(context, 14),
               ),
-              unselectedLabelColor: Color(0xFFB273FF),
+              unselectedLabelColor: const Color(0xFFB273FF),
               unselectedLabelStyle: Font.title14.copyWith(
                 fontSize: getResponsiveRatioByWidth(context, 14),
               ),
               splashFactory: NoSplash.splashFactory,
-              overlayColor: WidgetStatePropertyAll(Colors.transparent),
+              overlayColor: const WidgetStatePropertyAll(Colors.transparent),
               dividerHeight: 0,
             );
           },
