c11277cd3bbe4392e8fc0ac18e1849c0bc16f1ed
leehyunjin <skyhyunjinlee@gmail.com>
2025-06-17 17:15:22 +0900

    fix: dto 수정
    - 만약에 서버에서 받은 데이터나 필드 값이 null일 경우를 고려해서 nullable로 변경

--- lib/data/dtos/statistics_dto.dart
+++ lib/data/dtos/statistics_dto.dart
@@ -2,11 +2,13 @@ class StatisticsDto {
   StatisticsDto({
     this.month,
     this.year,
-    required this.frequency,
-    required this.distribution,
-    required this.moodState,
-    required this.keywords,
-  });
+    this.frequency = 0,
+    DreamScore? distribution,
+    MoodState? moodState,
+    List<Keyword>? keywords,
+  }) : distribution = distribution ?? DreamScore(),
+       moodState = moodState ?? MoodState(),
+       keywords = keywords ?? [];
 
   final String? month;
   final String? year;
@@ -15,15 +17,17 @@ class StatisticsDto {
   final MoodState moodState;
   final List<Keyword> keywords;
 
-  factory StatisticsDto.fromJson(Map<String, dynamic> json) => StatisticsDto(
-    month: json['MONTH'],
-    year: json['YEAR'],
-    frequency: json['FREQUENCY'],
-    distribution: DreamScore.fromJson(json['DISTRIBUTION']),
-    moodState: MoodState.fromJson(json['MOOD_STATE']),
-    keywords: List<Keyword>.from(
-      json['KEYWORDS']!.map((x) => Keyword.fromJson(x)),
-    ),
+  factory StatisticsDto.fromJson(Map<String, dynamic>? json) => StatisticsDto(
+    month: json?['MONTH'],
+    year: json?['YEAR'],
+    frequency: json?['FREQUENCY'] ?? 0,
+    distribution: DreamScore.fromJson(json?['DISTRIBUTION']),
+    moodState: MoodState.fromJson(json?['MOOD_STATE']),
+    keywords:
+        (json?['KEYWORDS'] as List?)
+            ?.map((x) => Keyword.fromJson(x as Map<String, dynamic>?))
+            .toList() ??
+        [],
   );
 
   Map<String, dynamic> toJson() => {
@@ -32,63 +36,69 @@ class StatisticsDto {
     'FREQUENCY': frequency,
     'DISTRIBUTION': distribution.toJson(),
     'MOOD_STATE': moodState.toJson(),
-    'KEYWORDS': List<dynamic>.from(keywords.map((x) => x.toJson())),
+    'KEYWORDS': keywords.map((x) => x.toJson()).toList(),
   };
 }
 
 class Keyword {
-  final String? keyword;
-  final int? count;
+  Keyword({this.keyword = '', this.count = 0});
 
-  Keyword({this.keyword, this.count});
+  final String keyword;
+  final int count;
 
-  factory Keyword.fromJson(Map<String, dynamic> json) =>
-      Keyword(keyword: json['KEYWORD'], count: json['COUNT']);
+  factory Keyword.fromJson(Map<String, dynamic>? json) =>
+      Keyword(keyword: json?['KEYWORD'] ?? '', count: json?['COUNT'] ?? 0);
 
   Map<String, dynamic> toJson() => {'KEYWORD': keyword, 'COUNT': count};
 }
 
 class MoodState {
-  final DreamScore? goodDream;
-  final DreamScore? ordinaryDream;
-  final DreamScore? badDream;
+  MoodState({
+    DreamScore? goodDream,
+    DreamScore? ordinaryDream,
+    DreamScore? badDream,
+  }) : goodDream = goodDream ?? DreamScore(),
+       ordinaryDream = ordinaryDream ?? DreamScore(),
+       badDream = badDream ?? DreamScore();
 
-  MoodState({this.goodDream, this.ordinaryDream, this.badDream});
+  final DreamScore goodDream;
+  final DreamScore ordinaryDream;
+  final DreamScore badDream;
 
-  factory MoodState.fromJson(Map<String, dynamic> json) => MoodState(
-    goodDream: DreamScore.fromJson(json['GOOD_DREAM']),
-    ordinaryDream: DreamScore.fromJson(json['ORDINARY_DREAM']),
-    badDream: DreamScore.fromJson(json['BAD_DREAM']),
+  factory MoodState.fromJson(Map<String, dynamic>? json) => MoodState(
+    goodDream: DreamScore.fromJson(json?['GOOD_DREAM']),
+    ordinaryDream: DreamScore.fromJson(json?['ORDINARY_DREAM']),
+    badDream: DreamScore.fromJson(json?['BAD_DREAM']),
   );
 
   Map<String, dynamic> toJson() => {
-    'GOOD_DREAM': goodDream!.toJson(),
-    'ORDINARY_DREAM': ordinaryDream!.toJson(),
-    'BAD_DREAM': badDream!.toJson(),
+    '길몽': goodDream.toJson(),
+    '일상몽': ordinaryDream.toJson(),
+    '흉몽': badDream.toJson(),
   };
 }
 
 class DreamScore {
+  DreamScore({
+    this.veryBad = 0,
+    this.bad = 0,
+    this.ordinary = 0,
+    this.good = 0,
+    this.veryGood = 0,
+  });
+
   final int veryBad;
   final int bad;
   final int ordinary;
   final int good;
   final int veryGood;
 
-  DreamScore({
-    required this.veryBad,
-    required this.bad,
-    required this.ordinary,
-    required this.good,
-    required this.veryGood,
-  });
-
-  factory DreamScore.fromJson(Map<String, dynamic> json) => DreamScore(
-    veryBad: json['VERY_BAD'],
-    bad: json['BAD'],
-    ordinary: json['ORDINARY'],
-    good: json['GOOD'],
-    veryGood: json['VERY_GOOD'],
+  factory DreamScore.fromJson(Map<String, dynamic>? json) => DreamScore(
+    veryBad: json?['VERY_BAD'] ?? 0,
+    bad: json?['BAD'] ?? 0,
+    ordinary: json?['ORDINARY'] ?? 0,
+    good: json?['GOOD'] ?? 0,
+    veryGood: json?['VERY_GOOD'] ?? 0,
   );
 
   Map<String, dynamic> toJson() => {
