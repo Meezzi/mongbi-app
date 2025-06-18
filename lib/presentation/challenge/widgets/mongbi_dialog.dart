3b9010de0ee72d99c33326b3cffaf421e2cbef8c
Meezzi <alswl-3016@naver.com>
2025-06-18 16:27:12 +0900

    feat: 챌린지 선택 후 몽비가 말하는 듯한 Dialog 위젯 구현

---
+++ lib/presentation/challenge/widgets/mongbi_dialog.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:mongbi_app/core/font.dart';
+import 'package:mongbi_app/presentation/auth/widgets/mongbi_image_widget.dart';
+import 'package:mongbi_app/presentation/common/floating_animation_widget.dart';
+import 'package:mongbi_app/presentation/dream/widgets/custom_button.dart';
+
+class MongbiDialog extends StatelessWidget {
+  const MongbiDialog({
+    super.key,
+    required this.content,
+    required this.buttonText,
+    required this.onSubmit,
+  });
+
+  final String content;
+  final String buttonText;
+  final VoidCallback onSubmit;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                content,
+                style: Font.title18.copyWith(color: Color(0xFF1A181B)),
+                textAlign: TextAlign.center,
+              ),
+              FloatingAnimationWidget(child: MongbiCharacter(size: 144)),
+              CustomButton(text: buttonText, onSubmit: onSubmit),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
