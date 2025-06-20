b82f7e660d9497b0e0eaf78a9ab7fdae65d7251f
Meezzi <alswl-3016@naver.com>
2025-06-20 17:58:23 +0900

    test: RemoteChallengeRepository 생성자에 named parameter 추가

--- test/data/challenge_repository_test.dart
+++ test/data/challenge_repository_test.dart
@@ -10,7 +10,7 @@ void main() {
 
   setUp(() {
     challengeDataSource = MockChallengeDataSource();
-    remoteChallengeRepository = RemoteChallengeRepository(challengeDataSource);
+    remoteChallengeRepository = RemoteChallengeRepository(challengeDataSource: challengeDataSource);
   });
 
   final dreamScore = 1;
