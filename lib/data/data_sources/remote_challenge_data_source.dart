9d11067728451b274714bbcc891ba6a4f5aed5b0
Meezzi <alswl-3016@naver.com>
2025-06-20 14:01:24 +0900

    feat: 꿈의 기분에 따른 챌린지를 가져오는 RemoteChallengeDataSource 구현

---
+++ lib/data/data_sources/remote_challenge_data_source.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+import 'package:mongbi_app/data/dtos/challenge_dto.dart';
+
+class RemoteChallengeDataSource {
+  RemoteChallengeDataSource({required this.dio});
+
+  final Dio dio;
+
+  Future<List<ChallengeDto>> fetchChallenge(int dreamScore) async {
+    try {
+      final response = await dio.post(
+        '/challenges/trait/$dreamScore',
+        data: dreamScore,
+      );
+
+      if (response.statusCode == 200 && response.data['success'] == true) {
+        final challengeList = List<Map<String, dynamic>>.from(
+          response.data['data'],
+        );
+
+        return challengeList.map((e) => ChallengeDto.fromJson(e)).toList();
+      } else {
+        throw Exception(response.data['message'] ?? '알 수 없는 오류가 발생하였습니다.');
+      }
+    } on DioException catch (e) {
+      throw Exception(e.message ?? '네트워크 오류가 발생하였습니다.');
+    }
+  }
+}
