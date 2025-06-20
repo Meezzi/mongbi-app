2f3cb932cc4b94162822f8626f212c0c9c288190
Meezzi <alswl-3016@naver.com>
2025-06-20 16:51:15 +0900

    feat: RemoteChallengeRepository 클래스에 ChallengeRepository 인터페이스 구현

--- lib/data/repositories/remote_challenge_repository.dart
+++ lib/data/repositories/remote_challenge_repository.dart
@@ -1,11 +1,13 @@
 import 'package:mongbi_app/data/data_sources/challenge_data_source.dart';
 import 'package:mongbi_app/domain/entities/challenge.dart';
+import 'package:mongbi_app/domain/repositories/challenge_repository.dart';
 
-class RemoteChallengeRepository {
+class RemoteChallengeRepository implements ChallengeRepository {
   RemoteChallengeRepository(this.challengeDataSource);
 
   final ChallengeDataSource challengeDataSource;
 
+  @override
   Future<List<Challenge>> fetchChallenge(int dreamScore) async {
     final challengeDtoList = await challengeDataSource.fetchChallenge(
       dreamScore,
