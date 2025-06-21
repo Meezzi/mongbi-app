4f006d260351326c864cd44d43ea6a5722328ada
Meezzi <alswl-3016@naver.com>
2025-06-21 22:16:11 +0900

    feat: 챌린지를 저장하는 RemoteSaveChallengeDataSource 구현

---
+++ lib/data/data_sources/remote_save_challenge_data_source.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+
+class RemoteSaveChallengeDataSource {
+  RemoteSaveChallengeDataSource({required this.dio});
+
+  final Dio dio;
+
+  Future<bool> saveChallenge({
+    required int uid,
+    required int challengeId,
+  }) async {
+    try {
+      final response = await dio.post(
+        '/dreams',
+        data: {'USER_IDX': uid, 'CHALLENGE_IDX': challengeId},
+      );
+
+      return response.statusCode == 201 && response.data['success'] == true;
+    } on DioException catch (e) {
+      throw Exception(e.message ?? '네트워크 오류가 발생했습니다.');
+    } catch (e) {
+      throw Exception('알수 없는 오류가 발생했습니다.');
+    }
+  }
+}
