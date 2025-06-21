0bb03baa273aebef636a3d1e751018c7d1f4b47e
Meezzi <alswl-3016@naver.com>
2025-06-21 20:05:59 +0900

    feat: ChallengeIntroPage를 ConsumerWidget으로 변경하고 챌린지 로딩 로직 실행

--- lib/presentation/challenge/challenge_intro_page.dart
+++ lib/presentation/challenge/challenge_intro_page.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mongbi_app/presentation/challenge/widgets/challenge_intro_mongbi_message_view.dart';
 import 'package:mongbi_app/presentation/common/action_button_row.dart';
+import 'package:mongbi_app/providers/challenge_provider.dart';
 
-class ChallengeIntroPage extends StatelessWidget {
+class ChallengeIntroPage extends ConsumerWidget {
   const ChallengeIntroPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       backgroundColor: Color(0xFFFAFAFA),
       body: SafeArea(
@@ -22,7 +24,11 @@ class ChallengeIntroPage extends StatelessWidget {
                 onLeftPressed: () {
                   context.go('/home');
                 },
-                onRightPressed: () {
+                onRightPressed: () async {
+                  // TODO: 사용자가 선택한 꿈 기분으로 변경
+                  await ref
+                      .read(challengeViewModelProvider.notifier)
+                      .loadChallenges(1);
                   context.go('/challenge');
                 },
               ),
