32743589a2c4d42b63d0e2cde59e2f9694e28add
Meezzi <alswl-3016@naver.com>
2025-06-21 22:56:36 +0900

    feat: SaveChallengeUseCase 구현

---
+++ lib/domain/use_cases/save_challenge_use_case.dart
@@ -0,0 +1,14 @@
+import 'package:mongbi_app/domain/repositories/challenge_repository.dart';
+
+class SaveChallengeUseCase {
+  SaveChallengeUseCase({required this.repository});
+
+  final ChallengeRepository repository;
+
+  Future<bool> saveChallenge({
+    required int uid,
+    required int challengeId,
+  }) async {
+    return await repository.saveChallenge(uid: uid, challengeId: challengeId);
+  }
+}
