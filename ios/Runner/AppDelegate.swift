7eaf7b7640e0497fa118088d45f000e9079f9aea
leehyunjin <skyhyunjinlee@gmail.com>
2025-06-23 17:44:23 +0900

    [Alarm] Android FCM 설정

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,7 +2,6 @@ import UIKit
 import Flutter
 import UserNotifications
 import NidThirdPartyLogin
-import FirebaseCore
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,26 +9,22 @@ import FirebaseCore
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
-
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self
     }
-
-    UIApplication.shared.registerForRemoteNotifications()
 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
   override func application(
-    _ application: UIApplication,
+    _ app: UIApplication,
     open url: URL,
     options: [UIApplication.OpenURLOptionsKey : Any] = [:]
   ) -> Bool {
     if NidOAuth.shared.handleURL(url) {
       return true
     }
-    return super.application(application, open: url, options: options)
+    return super.application(app, open: url, options: options)
   }
 }
