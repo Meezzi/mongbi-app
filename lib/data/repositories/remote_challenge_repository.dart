908188ba317bdf9a41364ba443b72d67785d8bf9
Meezzi <alswl-3016@naver.com>
2025-06-24 08:36:57 +0900

    feat: ChallengeRepository의 completeChallenge 메서드 구현

--- lib/data/repositories/remote_challenge_repository.dart
+++ lib/data/repositories/remote_challenge_repository.dart
@@ -1,3 +1,4 @@
+import 'package:mongbi_app/data/data_sources/complete_challenge_data_source.dart';
 import 'package:mongbi_app/data/data_sources/fetch_challenge_data_source.dart';
 import 'package:mongbi_app/data/data_sources/save_challenge_data_source.dart';
 import 'package:mongbi_app/domain/entities/challenge.dart';
@@ -7,10 +8,12 @@ class RemoteChallengeRepository implements ChallengeRepository {
   RemoteChallengeRepository({
     required this.challengeDataSource,
     required this.saveChallengeDataSource,
+    required this.completeChallengeDataSource,
   });
 
   final FetchChallengeDataSource challengeDataSource;
   final SaveChallengeDataSource saveChallengeDataSource;
+  final CompleteChallengeDataSource completeChallengeDataSource;
 
   @override
   Future<List<Challenge>> fetchChallenge(int dreamScore) async {
@@ -30,6 +33,18 @@ class RemoteChallengeRepository implements ChallengeRepository {
     return saveChallengeDataSource.saveChallenge(
       dreamId: dreamId,
       uid: uid,
+      challengeId: challengeId,
+    );
+  }
+
+  Future<bool> completeChallenge({
+    required int uid,
+    required int dreamId,
+    required int challengeId,
+  }) async {
+    return await completeChallengeDataSource.completeChallenge(
+      uid: uid,
+      dreamId: dreamId,
       challengeId: challengeId,
     );
   }
