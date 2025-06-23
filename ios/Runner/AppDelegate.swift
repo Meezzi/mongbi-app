2599f86f19c441d67d86e0f93d388acb70e1f9d0
Meezzi <alswl-3016@naver.com>
2025-06-23 10:49:13 +0900

    chore: FirebaseCore 추가

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,21 +2,21 @@ import UIKit
 import Flutter
 import UserNotifications
 import NidThirdPartyLogin
+import FirebaseCore
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure()
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self
     }
-
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-
   override func application(
     _ app: UIApplication,
     open url: URL,
