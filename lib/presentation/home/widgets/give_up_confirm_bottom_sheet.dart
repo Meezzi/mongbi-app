8c3e214fd1e89e12de2643b58b7f43303cfda156
Meezzi <alswl-3016@naver.com>
2025-06-24 22:09:50 +0900

    refactor: GiveUpConfirmBottomSheet을 BottomSheet 위젯에서 Container로 변경

--- lib/presentation/home/widgets/give_up_confirm_bottom_sheet.dart
+++ lib/presentation/home/widgets/give_up_confirm_bottom_sheet.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mongbi_app/core/font.dart';
 import 'package:mongbi_app/presentation/common/action_button_row.dart';
 
@@ -19,48 +18,43 @@ class GiveUpConfirmBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomSheet(
-      onClosing: () {},
-      builder: (context) {
-        return Container(
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(24),
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(24),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(height: 32),
+          Text(title, style: Font.title18),
+          Text(
+            subTitle,
+            style: Font.subTitle12.copyWith(color: Color(0xFF76717A)),
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SizedBox(height: 32),
-              Text(title, style: Font.title18),
-              Text(
-                subTitle,
-                style: Font.subTitle12.copyWith(color: Color(0xFF76717A)),
-              ),
-              SizedBox(height: 8),
-              SvgPicture.asset(
-                'assets/images/sad_mongbi.svg',
-                width: 144,
-                height: 144,
-                fit: BoxFit.contain,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                child: ActionButtonRow(
-                  leftText: '계속할래',
-                  rightText: '포기할래',
-                  onLeftPressed: () {
-                    onContinue();
-                  },
-                  onRightPressed: () {
-                    onGiveUp();
-                  },
-                ),
-              ),
-            ],
+          SizedBox(height: 8),
+          Image.asset(
+            'assets/images/sad_mongbi.webp',
+            width: 144,
+            height: 144,
           ),
-        );
-      },
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: ActionButtonRow(
+              leftText: '계속할래',
+              rightText: '포기할래',
+              onLeftPressed: () {
+                onContinue();
+              },
+              onRightPressed: () {
+                onGiveUp();
+              },
+            ),
+          ),
+          SizedBox(height: 48),
+        ],
+      ),
     );
   }
 }
