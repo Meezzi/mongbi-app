5294401600a329b31a3850cc376502a3cc961012
Meezzi <alswl-3016@naver.com>
2025-06-24 22:10:01 +0900

    refactor: CompletionBottomSheet을 BottomSheet 위젯에서 Container로 변경

--- lib/presentation/home/widgets/completion_bottom_sheet.dart
+++ lib/presentation/home/widgets/completion_bottom_sheet.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mongbi_app/core/font.dart';
 import 'package:mongbi_app/presentation/common/button_type.dart';
 import 'package:mongbi_app/presentation/common/filled_button_widget.dart';
@@ -20,43 +19,38 @@ class CompletionBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomSheet(
-      onClosing: () {},
-      builder: (context) {
-        return Container(
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(24),
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(24),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(height: 32),
+          Text(title, style: Font.title18),
+          Text(
+            subTitle,
+            style: Font.subTitle12.copyWith(color: Color(0xFF76717A)),
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SizedBox(height: 32),
-              Text(title, style: Font.title18),
-              Text(
-                subTitle,
-                style: Font.subTitle12.copyWith(color: Color(0xFF76717A)),
-              ),
-              SizedBox(height: 8),
-              SvgPicture.asset(
-                'assets/images/happy_mongbi.svg',
-                width: 144,
-                height: 144,
-                fit: BoxFit.contain,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                child: FilledButtonWidget(
-                  type: ButtonType.primary,
-                  text: '내가 꾼 꿈이야',
-                  onPress: onButtonPressed,
-                ),
-              ),
-            ],
+          SizedBox(height: 8),
+          Image.asset(
+            'assets/images/happy_mongbi.webp',
+            width: 144,
+            height: 144,
           ),
-        );
-      },
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: FilledButtonWidget(
+              type: ButtonType.primary,
+              text: '내가 꾼 꿈이야',
+              onPress: onButtonPressed,
+            ),
+          ),
+          SizedBox(height: 48),
+        ],
+      ),
     );
   }
 }
