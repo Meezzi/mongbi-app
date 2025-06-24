2144e324d2c9ae879cc70b7a783a2edf63fb1de7
leehyunjin <skyhyunjinlee@gmail.com>
2025-06-24 09:31:33 +0900

    style: 공통 버튼 스타일 클래스 활용하여 버튼 UI 변경

--- lib/presentation/alarm/widgets/delete_modal.dart
+++ lib/presentation/alarm/widgets/delete_modal.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mongbi_app/core/font.dart';
+import 'package:mongbi_app/presentation/common/button_type.dart';
+import 'package:mongbi_app/presentation/common/filled_button_widget.dart';
+import 'package:mongbi_app/presentation/common/ghost_button_widget.dart';
 
 class DeleteModal extends StatelessWidget {
   const DeleteModal({super.key});
@@ -37,71 +40,46 @@ class DeleteModal extends StatelessWidget {
                   ),
                   child: Row(
                     children: [
-                      _button(
-                        backgroundColor: Color(0xFFF4EAFF),
-                        foregroundColor: Color(0xFFB273FF),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color(0x191A181B),
-                            blurRadius: 10,
-                            offset: Offset(2, 2),
-                            spreadRadius: 0,
-                          ),
-                        ],
-                        text: '취소',
-                        onTap: () {
-                          context.pop();
-                        },
+                      Expanded(
+                        child: GhostButtonWidget(
+                          type: ButtonType.primary,
+                          text: '취소',
+                          onPress: () {
+                            context.pop();
+                          },
+                        ),
                       ),
                       SizedBox(width: 8),
-                      _button(
-                        backgroundColor: Color(0xFF8B2DFF),
-                        foregroundColor: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color(0x331A181B),
-                            blurRadius: 10,
-                            offset: Offset(2, 2),
-                            spreadRadius: 0,
-                          ),
-                        ],
-                        text: '전체 삭제',
-                        onTap: () {
-                          context.pop();
-                        },
+                      Expanded(
+                        child: FilledButtonWidget(
+                          type: ButtonType.primary,
+                          text: '전체 삭제',
+                          onPress: () {
+                            context.pop();
+                          },
+                        ),
                       ),
+                      // _button(
+                      //   backgroundColor: Color(0xFF8B2DFF),
+                      //   foregroundColor: Colors.white,
+                      //   boxShadow: [
+                      //     BoxShadow(
+                      //       color: Color(0x331A181B),
+                      //       blurRadius: 10,
+                      //       offset: Offset(2, 2),
+                      //       spreadRadius: 0,
+                      //     ),
+                      //   ],
+                      //   text: '전체 삭제',
+                      //   onTap: () {
+                      //     context.pop();
+                      //   },
+                      // ),
                     ],
                   ),
                 ),
               ],
             ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _button({
-    required Color backgroundColor,
-    required Color foregroundColor,
-    required List<BoxShadow> boxShadow,
-    required String text,
-    required VoidCallback onTap,
-  }) {
-    return Expanded(
-      child: GestureDetector(
-        onTap: onTap,
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: backgroundColor,
-            borderRadius: BorderRadius.circular(999),
-            boxShadow: boxShadow,
-          ),
-          child: Text(
-            text,
-            style: Font.title18.copyWith(color: foregroundColor),
           ),
         ),
       ),
