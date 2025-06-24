b3b2e136ce0299f9b2da7a23b5383481f2e7d563
MoonHo <75512752+monho@users.noreply.github.com>
2025-06-24 11:23:25 +0900

    fix:병합 에러 수정

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,7 @@ class _MyAppState extends ConsumerState<MyApp> with WidgetsBindingObserver {
       theme: ThemeData(fontFamily: 'NanumSquareRound'),
       builder: (context, child) {
         return MediaQuery(
-          data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+          data: MediaQuery.of(context).copyWith(textScaler: TextScaler.linear(1.0)),
           child: child!,
         );
       },
