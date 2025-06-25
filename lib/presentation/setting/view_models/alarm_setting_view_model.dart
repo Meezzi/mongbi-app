fa809dc777856e63c2889a02638c067fad1fccd4
MoonHo <75512752+monho@users.noreply.github.com>
2025-06-25 10:24:16 +0900

    fix: 뷰모델 및 뷰 수정 셋팅 내용 SharedPreferns에 저장 후 실시간 동기화

--- lib/presentation/setting/view_models/alarm_setting_view_model.dart
+++ lib/presentation/setting/view_models/alarm_setting_view_model.dart
@@ -1,26 +1,84 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mongbi_app/presentation/remind/view_model/remind_time_setting_view_model.dart';
 import 'package:mongbi_app/presentation/setting/models/alarm_setting_state.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AlarmSettingViewModel extends Notifier<AlarmSettingState> {
-  void toggleAll() {
-    state = AlarmSettingState.syncAll(!state.isAll);
-  }
-
-  void toggleReminder() {
-    state = state.copyWith(isReminder: !state.isReminder).recalculateIsAll();
-  }
-
-  void toggleChallenge() {
-    state = state.copyWith(isChallenge: !state.isChallenge).recalculateIsAll();
-  }
+  late SharedPreferences _prefs;
 
   @override
   AlarmSettingState build() {
-    // TODO: 초기 알림 설정할 때 했던 값으로 변경
+    _initPrefs();
     return AlarmSettingState(
       isAll: false,
       isReminder: false,
       isChallenge: false,
     );
+  }
+
+  Future<void> _initPrefs() async {
+    _prefs = await SharedPreferences.getInstance();
+
+    final isReminder = _prefs.getBool('alarm_isReminder') ?? false;
+    final isChallenge = _prefs.getBool('alarm_isChallenge') ?? false;
+
+    state = AlarmSettingState(
+      isReminder: isReminder,
+      isChallenge: isChallenge,
+      isAll: isReminder && isChallenge,
+    );
+  }
+
+  void toggleAll() {
+    final next = !state.isAll;
+    final nextState = AlarmSettingState(
+      isAll: next,
+      isReminder: next,
+      isChallenge: next,
+    );
+
+    state = nextState;
+    _saveState(nextState);
+
+    final notificationService = NotificationService();
+    if (next) {
+      notificationService.scheduleDailyReminder(
+        const TimeOfDay(hour: 8, minute: 0),
+      );
+    } else {
+      notificationService.cancelReminderNotification();
+    }
+  }
+
+  void toggleReminder() async {
+    final next = !state.isReminder;
+    final nextState = state.copyWith(isReminder: next).recalculateIsAll();
+
+    state = nextState;
+    _saveState(nextState);
+
+    final notificationService = NotificationService();
+    if (next) {
+      await notificationService.scheduleDailyReminder(
+        const TimeOfDay(hour: 8, minute: 0),
+      );
+    } else {
+      await notificationService.cancelReminderNotification();
+    }
+  }
+
+  void toggleChallenge() {
+    final nextState = state
+        .copyWith(isChallenge: !state.isChallenge)
+        .recalculateIsAll();
+
+    state = nextState;
+    _saveState(nextState);
+  }
+
+  void _saveState(AlarmSettingState s) {
+    _prefs.setBool('alarm_isReminder', s.isReminder);
+    _prefs.setBool('alarm_isChallenge', s.isChallenge);
   }
 }
