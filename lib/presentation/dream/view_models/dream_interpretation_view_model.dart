c216f0f999e13c4ab3d195357dadf33600d1861a
MoonHo <75512752+monho@users.noreply.github.com>
2025-06-27 19:33:10 +0900

    Add share_plus dependency and include share icon SVG
    - Added `share_plus` package to `pubspec.yaml` for sharing functionality.
    - Introduced a new SVG icon for sharing in `assets/icons/share.svg`.

--- lib/presentation/dream/view_models/dream_interpretation_view_model.dart
+++ lib/presentation/dream/view_models/dream_interpretation_view_model.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mongbi_app/domain/entities/dream.dart';
 import 'package:mongbi_app/presentation/dream/models/dream_interpretation_state.dart';
+import 'package:share_plus/share_plus.dart';
 
 class DreamInterpretationViewModel extends Notifier<DreamInterpretationState> {
   @override
@@ -19,5 +22,22 @@ class DreamInterpretationViewModel extends Notifier<DreamInterpretationState> {
       psychologicalStateKeywords: dream.psychologicalStateKeywords,
       mongbiComment: dream.mongbiComment,
     );
+  }
+
+  void shareDreamInterpretation(String dreamSummary) {
+    final storeLink =
+        Platform.isAndroid ? '' : 'https://apps.apple.com/app/id6747565007';
+
+    final message = '''
+🌙 [몽비] 오늘 꾼 꿈, 이런 의미래요!
+
+"$dreamSummary"
+
+👇 당신의 꿈도 해석해보세요!
+📱 몽비 앱 설치하기
+$storeLink
+''';
+
+    Share.share(message);
   }
 }
