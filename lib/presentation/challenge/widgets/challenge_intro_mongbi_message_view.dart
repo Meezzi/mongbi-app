c302102751af4b07b34ed87ee2e4d4bff03561c5
Meezzi <alswl-3016@naver.com>
2025-07-01 13:59:10 +0900

    refactor: 챌린지에 대한 설명 추가

--- lib/presentation/challenge/widgets/challenge_intro_mongbi_message_view.dart
+++ lib/presentation/challenge/widgets/challenge_intro_mongbi_message_view.dart
@@ -12,9 +12,15 @@ class MongbiMessageView extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(
-          '선물을 준비했몽, 골라봐!\n따라하면 기분이 좋아질거야!',
+          '선물을 준비했몽, 골라봐!',
           style: Font.title20.copyWith(color: Color(0xFF1A181B)),
           textAlign: TextAlign.center,
+        ),
+        SizedBox(height: 8),
+        Text(
+          '감정에 어울리는 작은 챌린지를 통해,\n하루를 기분 좋게 시작할 수 있어요.',
+          textAlign: TextAlign.center,
+          style: Font.body16.copyWith(color: Color(0xFF636363)),
         ),
         SizedBox(height: 48),
         FloatingAnimationWidget(child: MongbiCharacter(size: 288)),
