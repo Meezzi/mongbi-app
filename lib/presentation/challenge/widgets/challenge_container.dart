e7d6c94ea263e5382e4a5bd8e9b63e79d5c6eb67
Meezzi <alswl-3016@naver.com>
2025-07-01 16:05:28 +0900

    refactor: Transform을 AnimatedRotation으로 변경

--- lib/presentation/challenge/widgets/challenge_container.dart
+++ lib/presentation/challenge/widgets/challenge_container.dart
@@ -25,9 +25,11 @@ class ChallengeContainer extends StatelessWidget {
       scale: isSelected ? 1.1 : 1.0,
       duration: const Duration(milliseconds: 300),
       curve: Curves.easeInOut,
-      child: Transform(
-        alignment: Alignment.center,
-        transform: Matrix4.identity()..rotateZ(rotationAngle),
+      child: AnimatedRotation(
+        turns:
+            (isSelected ? 0.0 : rotationAngle),
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
         child: Container(
           width: screenHeight * 0.23,
           height: screenHeight * 0.23,
