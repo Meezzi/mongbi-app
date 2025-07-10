67e97c4aa01c9fb22e9a90d1017840ba3fedd2fb
MoonHo <75512752+monho@users.noreply.github.com>
2025-07-11 08:23:04 +0900

    feat: 엔티티 추가

---
+++ lib/domain/entities/subscriptionproduct.dart
@@ -0,0 +1,13 @@
+class SubscriptionProduct {
+
+  const SubscriptionProduct({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+  });
+  final String id;
+  final String title;
+  final String description;
+  final String price;
+}
