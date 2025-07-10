fd009d6a2c0b7afa7ebc38f21d99ec220e210653
MoonHo <75512752+monho@users.noreply.github.com>
2025-07-10 23:18:32 +0900

    fix: 스크롤뷰 적용

--- lib/presentation/payment/payment_page.dart
+++ lib/presentation/payment/payment_page.dart
@@ -32,57 +32,66 @@ class PaymentPage extends ConsumerWidget {
         backgroundColor: const Color(0xFFFCF6FF),
       ),
       backgroundColor: const Color(0xFFFCF6FF),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 55),
-              Image.asset(
-                'assets/images/medal.webp',
-                fit: BoxFit.cover,
-                width: 36,
-                height: 36,
-              ),
-              SizedBox(height: 10),
-              Text(
-                '7일간 무료로 먼저 체험해보세요! \n 결제는 언제든 취소할 수 있어요.',
-                textAlign: TextAlign.center,
-                style: Font.title14.copyWith(color: const Color(0xFF1A181B)),
-              ),
-              SizedBox(height: 50),
-              SubscriptionSelector(),
-              SizedBox(height: 15),
-              Text(
-                '*무료 구독은 첫 구매시에만 적용됩니다.',
-                textAlign: TextAlign.center,
-                style: Font.body12.copyWith(color: const Color(0xFF76717A)),
-              ),
-              SizedBox(height: 30),
-              PremiumBenefitCard(
-                title: '광고 완전 제거',
-                description: '몽비와 함께하는 시간동안\n광고는 없어드려요',
-                assetImage: 'assets/images/ad.webp',
-              ),
-              PremiumBenefitCard(
-                title: '프리미엄 해몽',
-                description: '보다 더 자세한 해몽을 통해\n내 꿈을 이해할 수 있어요',
-                assetImage: 'assets/images/ad.webp',
-              ),
-              PremiumBenefitCard(
-                title: '심층 퍼스널 리포트',
-                description: '더욱 자세한 꿈 분석을 통해\n나의 심리를 더 자세히 알 수 있어요.',
-                assetImage: 'assets/images/ad.webp',
-              ),
-              SizedBox(height: 20),
-              RestorePurchaseButton(
-                onPressed: () {
-                  // TODO: 구매 복원 로직
-                  print('구매 복원 시도!');
-                },
-              ),
-            ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 55),
+                Image.asset(
+                  'assets/images/medal.webp',
+                  fit: BoxFit.cover,
+                  width: 36,
+                  height: 36,
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  '7일간 무료로 먼저 체험해보세요! \n 결제는 언제든 취소할 수 있어요.',
+                  textAlign: TextAlign.center,
+                  style: Font.title14.copyWith(color: const Color(0xFF1A181B)),
+                ),
+                const SizedBox(height: 50),
+                const SubscriptionSelector(),
+                const SizedBox(height: 15),
+                Text(
+                  '*무료 구독은 첫 구매시에만 적용됩니다.',
+                  textAlign: TextAlign.center,
+                  style: Font.body12.copyWith(color: const Color(0xFF76717A)),
+                ),
+                const SizedBox(height: 30),
+                const PremiumBenefitCard(
+                  title: '광고 완전 제거',
+                  description: '몽비와 함께하는 시간동안\n광고는 없어드려요',
+                  assetImage: 'assets/images/ad.webp',
+                ),
+                const PremiumBenefitCard(
+                  title: '프리미엄 해몽',
+                  description: '보다 더 자세한 해몽을 통해\n내 꿈을 이해할 수 있어요',
+                  assetImage: 'assets/images/ad.webp',
+                ),
+                const PremiumBenefitCard(
+                  title: '심층 퍼스널 리포트',
+                  description: '더욱 자세한 꿈 분석을 통해\n나의 심리를 더 자세히 알 수 있어요.',
+                  assetImage: 'assets/images/ad.webp',
+                ),
+                const SizedBox(height: 20),
+                RestorePurchaseButton(
+                  onPressed: () {
+                    // TODO: 구매 복원 로직
+                    print('구매 복원 시도!');
+                  },
+                ),
+                const SizedBox(height: 15),
+                Text(
+                  '구글 플레이스토어에 로그인된 계정을 통해 결제가 진행됩니다.\n연간 맴버쉽의 가격은 연 ₩33,000원 으로 매년 자동으로 결제되며 \n언제든 취소가 가능합니다.',
+                  textAlign: TextAlign.center,
+                  style: Font.body12.copyWith(color: const Color(0xFF76717A)),
+                ),
+                const SizedBox(height: 20),
+              ],
+            ),
           ),
         ),
       ),
