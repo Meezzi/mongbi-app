21d9321fb78f9bcaef6726ee0f05b8de3d2841cc
Meezzi <alswl-3016@naver.com>
2025-11-24 02:05:23 +0900

    refactor: terms 관련 파일을 feature/terms 폴더 내로 이동

--- lib/features/terms/providers/terms_provider.dart
+++ lib/features/terms/providers/terms_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mongbi_app/data/repositories/remote_terms_repository.dart';
 import 'package:mongbi_app/features/terms/data/data_sources/remote_terms_data_source.dart';
+import 'package:mongbi_app/features/terms/data/repositories/remote_terms_repository.dart';
 import 'package:mongbi_app/features/terms/domain/repositories/terms_repository.dart';
 import 'package:mongbi_app/features/terms/domain/use_cases/agree_to_terms_usecase.dart';
 import 'package:mongbi_app/features/terms/domain/use_cases/fetch_terms_use_case.dart';
